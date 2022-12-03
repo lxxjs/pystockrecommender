2abb50a9f1dfa3c32c7d0dcaa4fe37bf98bf4a5c
Jongseo Lee <james0614ll@gmail.com>
2022-12-03 14:25:45 +0000

    Add function getDiscountRate

--- main.py
+++ main.py
@@ -10,15 +10,31 @@
 """
 
 import requests
+from bs4 import BeautifulSoup
 
-api_key = 'b64695f3f2a79d07bde772ffa630f935e0d050c0'
+discountRateLocation: int  = 87 #BBB- 5y
+KISlink: str = 'https://www.kisrating.com/ratingsStatistics/statics_spread.do'
 
+DART_API_Key = 'b64695f3f2a79d07bde772ffa630f935e0d050c0'
 samsung = '00126380'
+
+
+def getDiscountRate(link, location) -> float:
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, "html.parser")
+    result = float(soup.select('td.ar.pr40')[location].text)
+    return result
+
+
 
 #res = requests.get('https://opendart.fss.or.kr/api/company.json', params={'crtfc_key' : api_key, 'corp_code' : samsung})
 res = requests.get('https://www.fnspace.com/Api/FinanceApi?key=sample&format=json&code=A005930&item=M111000,M111100,M111600,M113000&consolgb=M&annualgb=A&fraccyear=2014&toaccyear=2018')
 res.raise_for_status()
 print(res)
 data = res.json()
+
+a = getDiscountRate(KISlink, discountRateLocation)
+print(a)
+
 
 print(data)
